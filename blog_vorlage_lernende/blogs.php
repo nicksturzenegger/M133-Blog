5ee9d2d73319160de59d7b29b7ff3aba6ce2300c
nicksturzenegger <nick@sturzis.com>
2017-11-09 15:20:10 +0100

    foreach schleife gemacht

--- blog_vorlage_lernende/blogs.php
+++ blog_vorlage_lernende/blogs.php
@@ -8,12 +8,7 @@
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blogs und der Ausgabe mit PHP ersetzt werden
   $blogs = getUserNames();
   foreach ($blogs as $blog){
-  	echo $blog['uid'];
-  	echo $blog['name'];
+  	echo "<div><a href='index.php?function=blogs&bid=" . $blog['uid'] . "title='Blog auswählen'><h4>" . $blog['name'] . "</h4></a></div>";
   }
 ?>
-	<div><a href='index.php?function=blogs&bid=4' title='Blog auswählen'><h4>Anna Abegglen</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=2' title='Blog auswählen'><h4>Hans Hinterseer</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=1' title='Blog auswählen'><h4>Marc Muster</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=3' title='Blog auswählen'><h4>Sonja Sauser</h4></a></div>
-
+	
